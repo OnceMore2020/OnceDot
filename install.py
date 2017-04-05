282e81229a9928678de99db9f624ea85de3ff71f
OnceMore2020 <guanhwang2011@gmail.com>
2017-04-05 13:16:03 +0800

    update profiles

--- install.py
+++ install.py
@@ -3,10 +3,15 @@
 
 import os
 
+OMZ_REPO = 'https://github.com/robbyrussell/oh-my-zsh.git'
+VP_REPO = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
 def main():
-    install_list = input('Choose the dotfiles you want to install:(e.g. 1,2,4)\n1):tmux 2):zsh 3):neovim\n')
+    install_list = raw_input('Choose the dotfiles you want to install: \
+                         (e.g. 1,2,4)\n1):tmux 2):zsh 3):neovim\n')
     items = install_list.split(',')
-    if(len(items)==0):
+    if len(items) == 0:
         print('No item selected!')
         return
     work_dir = os.getcwd()
@@ -14,31 +19,34 @@ def main():
     for item in items:
         if item == '1':
             print('Configuring tmux...')
-            os.system('rm -rf ' + home_dir + '/.tmux.conf')
-            os.system('ln -sf ' + work_dir + '/tmux/_tmux.conf ' + home_dir +'/.tmux.conf')
-            print('tmux configuration file created at ~/.tmux.conf')
-            print('-----------------------------------------------')
+            os.system('rm -rf ' + os.path.join(home_dir, '.tmux.conf'))
+            source_conf = os.path.join(work_dir, 'tmux/_tmux.conf')
+            target_conf = os.path.join(home_dir, '.tmux.conf')
+            os.system('ln -sf ' + source_conf + ' ' + target_conf)
         if item == '2':
             print('Configuring zsh...')
             print('Installing oh-my-zsh...')
-            os.system('rm -rf ' + home_dir + '/.oh-my-zsh')
-            os.system('rm -f ' + home_dir + '/.zshrc')
-            os.system('rm -f ' + home_dir + '/.zsh_aliases')
-            os.system('git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh')
-            os.system('ln -sf ' + work_dir + '/zsh/.zshrc ' + home_dir +'/.zshrc')
-            os.system('ln -sf ' + work_dir + '/zsh/.zsh_aliases ' + home_dir +'/.zsh_aliases')
-            os.system('curl -fLo ' + home_dir + '/.oh-my-zsh/themes/lambda-mod.zsh-theme https://github.com/halfo/lambda-mod-zsh-theme/raw/master/lambda-mod.zsh-theme')
+            os.system('rm -rf ' + os.path.join(home_dir, '.oh-my-zsh'))
+            os.system('rm -f ' + os.path.join(home_dir, '.zshrc'))
+            os.system('rm -f ' + os.path.join(home_dir, '.zsh_aliases'))
+            os.system('git clone --depth=1 ' + OMZ_REPO +
+                      ' ' + os.path.join(home_dir, '.oh-my-zsh'))
+            os.system('ln -sf ' + os.path.join(work_dir, 'zsh/_zshrc') +
+                      ' ' + os.path.join(home_dir, '.zshrc'))
+            os.system('ln -sf ' + os.path.join(work_dir, 'zsh/_zsh_aliases') +
+                      ' ' + os.path.join(home_dir, '.zsh_aliases'))
             print('Changing default shell(input your password to continue):')
             os.system('chsh -s /usr/bin/zsh')
-            print('zsh configuration file created at ~/.zshrc, ~/.zsh_aliases and ~/.oh-my-zsh')
-            print('---------------------------------------------------------------------------')
         if item == '3':
             print('Configuring neovim...')
             os.system('rm -rf ' + home_dir + '/.config/nvim')
             print('Installing vim-plug...')
-            os.system('curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
-            os.system('nvim -u ' + work_dir + '/neovim/.plugins ' + work_dir + '/neovim/.helptext')
-            os.system('ln -sf ' + work_dir + '/neovim/.init.vim ' + home_dir + '/.config/nvim/init.vim')
+            os.system('curl -fLo ~/.config/nvim/autoload/plug.vim ' +
+                      '--create-dirs ' + VP_REPO)
+            os.system('nvim -u ' + work_dir + '/neovim/.plugins ' +
+                      work_dir + '/neovim/.helptext')
+            os.system('ln -sf ' + work_dir + '/neovim/.init.vim ' +
+                      home_dir + '/.config/nvim/init.vim')
             os.system('nvim +UpdateRemotePlugins +qall')
             print('neovim configuration file created at ~/.config/nvim')
             print('-----------------------------------------------------')
